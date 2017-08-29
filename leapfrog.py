092542365389f6af6d4c3fba261ea65ac205c3df
grantmwilliams <grant.williams@okstate.edu>
2017-08-29 16:59:46 -0500

    updated leapfrog to match research paper exactly

--- leapfrog.py
+++ leapfrog.py
@@ -2,54 +2,99 @@ import numpy as np
 from test_functions import *
 from utilities import *
 
-def getFitness(xy, fitness, f):
-    for idx, val in enumerate(xy.A):
-        fitness[idx] = f(val[0], val[1])
-
-    return fitness
-
 @Counter.count
-def leapfrog(f, players=10, tol=1e-7, max_iter=1000, verbose=False, plotting=False):
-    xy_range = get_range(f) # get range from function
+def leapfrog(f, players=20, tol=1e-7, max_iter=1000, verbose=False, plotting=False):
 
-    fitness = np.zeros(players) # initialize fitness array
+    xy_range = get_range(f)
 
-    # scatter players across search space
-    xy = np.matrix(np.column_stack((np.random.uniform(xy_range[0], xy_range[1],
-                                                 size=players),np.random.uniform(xy_range[2],
-                                                                                 xy_range[3], size=players))))
-    # calc initial fitness
-    fitness = getFitness(xy, fitness, f)
+    # initialize two arrays to hold our players for x and y coordinates
+    px = np.zeros(players)
+    py = np.zeros(players)
 
-    # calc initial best values
-    bestidx = np.argmin(fitness)
-    best = fitness[bestidx]
-    last = best
-    for i in range(0, max_iter):
+    # initialize fitness function for each player
+    pf = np.zeros(players)
 
-        # get worst player
-        worstidx = np.argmax(fitness)
-        worst = fitness[worstidx]
+    # create initial conditions
+    for i in range(0, players):
+        x = np.random.uniform(xy_range[0], xy_range[1])
+        y = np.random.uniform(xy_range[2], xy_range[3])
+        test_val = f(x, y)
 
-        # worst leaps over best value
-        xy[worstidx][0,0] = xy[bestidx][0,0] - np.random.random_sample(size=1) * (xy[worstidx][0,0] - xy[bestidx][0,0])
-        xy[worstidx][0,1] = xy[bestidx][0,1] - np.random.random_sample(size=1) * (xy[worstidx][0,1] - xy[bestidx][0,1])
+        px[i] = x
+        py[i] = y
+        pf[i] = test_val
 
-        # get new fitness
-        fitness[worstidx] = f(xy[worstidx][0,0], xy[worstidx][0,1])
+    # now we can begin the main loop
+    for iteration in range(0, max_iter):
+        # get the highest(worst) and lowest(best) players:
 
-        if np.amin(fitness) < best:
-            last = best
-            best = np.amin(fitness)
-            bestidx = np.argmin(fitness)
+        # initialize worst player randomly which helps incase the floor is flat (like easom function)
+        Nhigh = np.random.randint(0, players) # we assign one as the worst player randomly
+        fhigh = pf[Nhigh]
 
-            if np.abs(best - last) < tol:
-                break
+        #initialize best player as first sport incase floor is flat
+        Nlow = 1
+        flow = pf[Nlow]
+        for p in range(0, players):
+            test_val = pf[p]
 
+            if test_val > fhigh:
+                Nhigh = p
+                fhigh = test_val
+
+            if test_val < flow:
+                Nlow = p
+                flow = test_val
+
+        # store x and y high values
+        xhigh = px[Nhigh]
+        yhigh = py[Nhigh]
+
+        # store x and y low values
+        xlow = px[Nlow]
+        ylow = py[Nlow]
+
+        # begin leapfrogging process
+        dx = xlow - xhigh
+        dy = ylow - yhigh
+
+        # jump for x and y directions separately
+        xhigh = xlow - np.random.uniform() * (xhigh - xlow)
+        yhigh = ylow - np.random.uniform() * (yhigh - ylow)
+        test_val = f(xhigh, yhigh)
+
+        # update arrays
+        px[Nhigh] = xhigh
+        py[Nhigh] = yhigh
+        pf[Nhigh] = test_val
+
+        # if we have a new best (leap was successful
+        if test_val < flow:
+            Nlow = Nhigh
+            flow = pf[Nlow]
+            xlow = xhigh
+            ylow = yhigh
+
+        if test_val <= fhigh:
+            # initialize worst player randomly which helps incase the floor is flat (like easom function)
+            Nhigh = np.random.randint(0, players)  # we assign one as the worst player randomly
+            fhigh = pf[Nhigh]
+
+            for k in range(0, players):
+                test_val = pf[k]
+
+                # if new worst
+                if test_val > fhigh:
+                    Nhigh = k
+                    fhigh = test_val
+
+            xhigh = px[Nhigh]
+            yhigh = py[Nhigh]
+
+        if (np.abs(dx) < tol or np.abs(dy) < tol):
+            break
 
     if verbose:
-        return best, (xy[bestidx][0,0], xy[bestidx][0,1])
-
+        return flow, (xlow, ylow)
     else:
-        return best
-    #return best, xy[bestidx]
+        return flow
